11189b0e94b0eb508e9df95392d6d4950a8910c9
Ryan Holanda <ryanholanda08@gmail.com>
2025-07-15 20:07:31 -0300

    fix: zup dropdown appearing in the incorrect place when opening after scroll

--- lib/buttons/zup_dropdown.dart
+++ lib/buttons/zup_dropdown.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:zup_core/zup_core.dart';
 import 'package:zup_ui_kit/src/pop_up_menu_item_wrapper.dart';
 
 /// Show a dropdown from anywhere by calling the static method [ZupDropdown.show]
@@ -8,17 +7,34 @@ class ZupDropdown {
   ///
   /// @param `showBelowContext` -> the context that the dropdown should be displayed below, usually the context of a button
   /// @param `child` -> the content that the dropdown will show
-  static Future<void> show({required BuildContext showBelowContext, required Widget child, Offset? offset}) {
-    return showMenu(
-      context: showBelowContext,
-      position: showBelowContext.relativeRect(
-        adjustOffsetBy: offset ?? const Offset(0, 5),
-        relativePosition: RelativePosition.below,
-      ),
+  static Future<void> show({
+    required BuildContext showBelowContext,
+    required Widget child,
+    Offset? offset,
+    bool useRootNavigator = true,
+  }) async {
+    final navigator = Navigator.of(showBelowContext, rootNavigator: useRootNavigator);
+    final overlayBox = navigator.overlay?.context.findRenderObject() as RenderBox?;
+    final targetBox = showBelowContext.findRenderObject() as RenderBox?;
+
+    if (targetBox == null || overlayBox == null || !targetBox.attached || !overlayBox.attached) return;
+
+    final position = RelativeRect.fromRect(
+      targetBox.localToGlobal(
+            Offset(0, targetBox.size.height) + (offset ?? Offset.zero),
+            ancestor: overlayBox,
+          ) &
+          targetBox.size,
+      Offset.zero & overlayBox.size,
+    );
+
+    await showMenu(
+      context: navigator.context,
+      position: position,
       menuPadding: EdgeInsets.zero,
       constraints: const BoxConstraints(),
       color: Colors.transparent,
-      useRootNavigator: true,
+      useRootNavigator: useRootNavigator,
       elevation: 0,
       items: [
         PopupMenuItemWrapper(
