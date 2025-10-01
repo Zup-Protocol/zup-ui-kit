7a7b685e476da204e89ab4d1c4998c46e4419373
Ryan Holanda <ryanholanda08@gmail.com>
2025-10-01 15:03:34 -0300

    feat: allow passing custom color to zup divider

--- lib/zup_divider.dart
+++ lib/zup_divider.dart
@@ -4,13 +4,16 @@ import 'package:zup_ui_kit/zup_theme_colors.dart';
 
 class ZupDivider extends StatelessWidget {
   /// Show a divider from the Zup UI Kit
-  const ZupDivider({super.key});
+  const ZupDivider({super.key, this.color});
+
+  /// Optionally pass a color to the divider line, otherwise it will use [ZupThemeColors.borderOnBackground]
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       widthFactor: 0.4,
-      child: Divider(color: ZupThemeColors.borderOnBackground.themed(context.brightness)),
+      child: Divider(color: color ?? ZupThemeColors.borderOnBackground.themed(context.brightness)),
     );
   }
 }
--- test/golden_test/zup_divider_test.dart
+++ test/golden_test/zup_divider_test.dart
@@ -1,13 +1,22 @@
-import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:zup_ui_kit/zup_divider.dart';
 
 import '../golden_config.dart';
 
 void main() {
-  Future<DeviceBuilder> goldenBuilder() async => await goldenDeviceBuilder(const Center(child: ZupDivider()));
+  Future<DeviceBuilder> goldenBuilder({Color? color}) async =>
+      await goldenDeviceBuilder(Center(child: ZupDivider(color: color)));
 
   zGoldenTest("Zup divider", goldenFileName: "zup_divider", (tester) async {
     return tester.pumpDeviceBuilder(await goldenBuilder());
   });
+
+  zGoldenTest(
+    "When passing the color argument, the passed color should be applied",
+    goldenFileName: "zup_divider_custom_color",
+    (tester) async {
+      await tester.pumpDeviceBuilder(await goldenBuilder(color: Colors.blue));
+    },
+  );
 }
