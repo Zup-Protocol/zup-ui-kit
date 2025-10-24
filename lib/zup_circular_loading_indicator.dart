e13c0343aa1a892f684dd1c056feb5b03a19df9a
Ryan Holanda <ryanholanda08@gmail.com>
2025-10-24 18:53:56 -0300

    chore: add docs to zup circular loading indicator and change default values

--- lib/zup_circular_loading_indicator.dart
+++ lib/zup_circular_loading_indicator.dart
@@ -2,18 +2,28 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:zup_core/zup_core.dart';
 import 'package:zup_ui_kit/zup_ui_kit.dart';
 
+/// Show a circular loading indicator from the Zup UI Kit.
+///
+/// Similar to [CircularProgressIndicator] but with some adaptations
 class ZupCircularLoadingIndicator extends StatelessWidget {
-  const ZupCircularLoadingIndicator({
-    super.key,
-    this.backgroundColor,
-    this.indicatorColor,
-    required this.size,
-  });
+  /// Show a circular loading indicator from the Zup UI Kit.
+  ///
+  /// Similar to [CircularProgressIndicator] but with some adaptations
+  const ZupCircularLoadingIndicator({super.key, this.trackColor, this.indicatorColor, required this.size});
 
-  final Color? backgroundColor;
+  /// Optionally change the track color, the part that the indicator is on, defaults
+  /// to [ZupColors.gray5] for light themes and [ZupColors.black4] for dark themes
+  final Color? trackColor;
+
+  /// Optionally change the indicator color, the rotating part, defaults to primary color of the theme
   final Color? indicatorColor;
+
+  /// The size of the circular loading indicator widget, required to be passed.
+  ///
+  /// This size will be used as both the width and height of the widget.
   final double size;
 
   @override
@@ -23,9 +33,9 @@ class ZupCircularLoadingIndicator extends StatelessWidget {
       width: size,
       child: CircularProgressIndicator(
         value: (!kIsWeb && Platform.environment.containsKey('FLUTTER_TEST')) ? 0.5 : null,
-        strokeWidth: size / 13,
-        backgroundColor: (backgroundColor ?? ZupColors.gray5).withValues(alpha: 0.2),
-        color: (indicatorColor ?? ZupColors.gray),
+        strokeWidth: size / 20,
+        backgroundColor: trackColor ?? (context.brightness.isDark ? ZupColors.black4 : ZupColors.gray5),
+        color: indicatorColor ?? Theme.of(context).primaryColor,
       ),
     );
   }
